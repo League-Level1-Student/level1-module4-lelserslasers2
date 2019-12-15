0595d009d3f61c0e4523b65ba6293de92ce0a3ce
League Student <league@iMac-24.attlocal.net>
2019-12-15 12:55:30 -0800

    Finished NastySuprise!!!
    :+1:

--- src/_02_simon_says/SimonSays.java
+++ src/_02_simon_says/SimonSays.java
@@ -30,7 +30,8 @@ public class SimonSays extends KeyAdapter {
 
 	// Complete steps 1 - 7 before you test
 	// 1. Declare a JFrame variable
-
+	JFrame frame = new JFrame();
+	
 	public void run() {
 		// 2. Add the four images that match keyboard keys like this:
 		// images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
